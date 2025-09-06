c98322b8ca6a1d281facfa20bec5616fdbd1db71
ivang934 <163324461+ivang934@users.noreply.github.com>
2025-09-05 21:41:20 -0300

    Delete scripts directory

--- scripts/getdates.js
+++
@@ -1,6 +0,0 @@
-
-document.getElementById("currentyear").textContent = new Date().getFullYear();
-
-
-document.getElementById("lastModified").textContent = 
-  "Last Modification: " + document.lastModified;
