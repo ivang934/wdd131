31245de2264ca0e765f724a444c7c2f569d4cd9c
ivangonzalez934 <igonzalezramos@byupathway.edu>
2026-03-07 18:22:18 -0300

    Last commit

---
+++ scripts/getdates.js
@@ -0,0 +1,7 @@
+
+const currentYearElement = document.querySelector("#currentyear");
+const today = new Date();
+currentYearElement.textContent = today.getFullYear();
+
+const lastModifiedElement = document.querySelector("#lastModified");
+lastModifiedElement.textContent = `Last Modification: ${document.lastModified}`;
